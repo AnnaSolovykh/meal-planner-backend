0fe5052ac0b6b82176315fbd68dce911cc4ed5e1
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-11-16 10:38:12 +0300

    add pagination

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -24,6 +24,12 @@ const getAllMeals = asyncWrapper(async (req, res) => {
         result = result.sort('createdAt');
     }
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
     const meals = await result;
     res.status(200).json({ meals });
 });
