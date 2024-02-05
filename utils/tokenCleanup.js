f526c3a2a7f507c587c9ba9b432325e7393cd4fb
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-02-05 19:40:37 +0300

    add token blacklisting functionality

---
+++ utils/tokenCleanup.js
@@ -0,0 +1,6 @@
+const cron = require('node-cron');
+const { removeExpiredTokens } = require('./tokenUtils');
+
+cron.schedule('0 0 * * *', async () => {
+    await removeExpiredTokens();
+});
---
+++ utils/tokenUtils.js
@@ -0,0 +1,7 @@
+const TokenBlacklist = require('./models/TokenBlacklist'); // 
+
+const removeExpiredTokens = async () => {
+    await TokenBlacklist.deleteMany({ expiresAt: { $lt: new Date() } });
+};
+
+module.exports = removeExpiredTokens;
